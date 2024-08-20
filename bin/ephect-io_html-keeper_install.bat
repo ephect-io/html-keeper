962cd93681ce04f723019a8ca5fba8f088667e4c
dpjb <dpjb@hotmail.fr>
2024-08-20 20:13:20 +0200

    Move all HtmlSaver files from framework to dedicated module.

---
+++ bin/ephect-io_html-keeper_install.bat
@@ -0,0 +1,4 @@
+@echo off
+
+cd vendor\ephect-io\html-keeper
+php use install:plugin %cd% %1 %2
---
+++ bin/ephect-io_html-keeper_install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+cd vendor/ephect-io/html-keeper
+php use install:plugin $(pwd) $1 $2
